698e3accfafeebde977533ca18317cf88d155f2b
dilermando <dilermandojmm@gmail.com>
2026-05-22 17:30:24 -0300

    fiz aula Condições (Parte 1) - Curso JavaScript #11

---
+++ aula11/ex008.js
@@ -0,0 +1,11 @@
+//console.log('O console funcionou corretamente!')
+const vel = 12
+console.log(`A velocidaee do sou carro e de ${vel} Km/h`)
+if (vel > 60) {             // Condicão simples!
+    console.log(`Vocé ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando o sinto de segurança!`)
+
+
+
+console.log(``) 
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+ const pais = 'Brasil'
+ console.log(`Vivendo em ${pais} .`)
+ if (pais !=  'Brasil') {
+    console.log('Voce e estranjeiro !')
+ } else {
+    console.log ('Voce e de terras Tupiniquim !')
+ }
