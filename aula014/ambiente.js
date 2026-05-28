6e23ec4eee3c70e5f60cdf87ea0366143f94a14f
dilermando <dilermandojmm@gmail.com>
2026-05-28 15:33:46 -0300

    fiz a aula14

--- aula014/ambiente.js
+++ aula014/ambiente.js
@@ -1,24 +1,5 @@
 
-/* 
-let c = 1;
-while (c <= 5) {
-    console.log(c);
-    c++
-} */
-
-//-------------------------
-
-/* 
-let c = 1;
-do {
-    console.log(c);
-    c++
-} while (c <= 5)  */
-
-//-------------------------
-
 console.log('Vai começar!');
-//  Inicialização ; Teste logico ; incremento
 for(  let c = 1   ;     c <= 5   ;    c++     ){
     console.log(c);
     
